fbcb1fd1b29b7e076582b20d0a722622dfa6586d
willian <willian.araujo1994@gmail.com>
2024-06-24 11:38:42 -0300

    fix: files

--- src/server.js
+++ src/server.js
@@ -5,7 +5,6 @@ const app = express();
 app.use(express.json());
 app.use(routes);
 
-const PORT = 3334;
 
 app.use((error, request, response, next) => {
   if (error instanceof Error) {
@@ -13,13 +12,15 @@ app.use((error, request, response, next) => {
       error: error.message,
     });
   }
-
+  
   console.error(error);
   return response.status(500).json({
     error: "Internal server error",
   });
 });
 
+
+const PORT = 3333;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
