c06b1376953b0ec8560c8f99d25b15c20b9ec621
willian <willian.araujo1994@gmail.com>
2024-07-07 15:25:55 -0300

    feat: new validation user middleware
    - added new middleware to dish routes

---
+++ src/middlewares/verifyUserAuthorization.js
@@ -0,0 +1,14 @@
+const AppError = require('../utils/AppError');
+function verifyUserAuthorization(roleToVerify) {
+    return (req, res, next) => {
+        const { role } = req.user;
+
+        if (!roleToVerify.includes(role)) {
+            throw new AppError("Acesso não autorizado", 401);
+        }
+
+        return next();
+    }
+}
+
+module.exports = verifyUserAuthorization;
