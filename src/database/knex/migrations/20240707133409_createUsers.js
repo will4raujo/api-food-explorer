cbb7263983f75c51c0ca1bee760908493b484ab5
willian <willian.araujo1994@gmail.com>
2024-07-09 13:58:39 -0300

    feat: populed database with default images
    - created a seed to populate dishs and default users

--- src/database/knex/migrations/20240707133409_createUsers.js
+++ src/database/knex/migrations/20240707133409_createUsers.js
@@ -9,7 +9,27 @@ exports.up = knex => knex.schema.createTable('users', table => {
   .notNullable().defaultTo("customer")
   table.timestamp("created_at").default(knex.fn.now())
   table.timestamp("updated_at").default(knex.fn.now())
+}).then(() => {
+  return knex("users").insert([
+    {
+      name: "Usuário Admin",
+      email: "admin@teste.com",
+      password: "123456",
+      role: "admin",
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now()
+    },
+    {
+      name: "Customer",
+      email: "customer@teste.com",
+      password: "123456",
+      role: "customer",
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now()
+    },
+  ])
 })
+
 
 
 exports.down = knex => knex.schema.dropTable("users")
