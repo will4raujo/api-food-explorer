078e5d25a177b7c4d57f2115d77d1fe415196ecd
willian <willian.araujo1994@gmail.com>
2024-07-09 18:59:05 -0300

    fix: caterory now is a enum

--- src/database/knex/migrations/20240704165022_createDishes.js
+++ src/database/knex/migrations/20240704165022_createDishes.js
@@ -1,7 +1,9 @@
 exports.up = knex => knex.schema.createTable('dishes', table => {
     table.increments('id')
     table.text('name').notNullable()
-    table.text('category').notNullable()
+    table.enum('category', ['meals', 'desserts', 'drinks']).notNullable()
+    .defaultTo('meals')
+    
     table.text('description').notNullable()
     table.decimal('price').notNullable()
     table.integer('user_id').references('id').inTable('users')
@@ -14,7 +16,7 @@ exports.up = knex => knex.schema.createTable('dishes', table => {
     return knex('dishes').insert([
         {
             name: 'Salada Ravanello',
-            category: 'refeições',
+            category: 'meals',
             description: 'Rabanetes, folhas verdes, molho agridoce salpicado com gergelim.',
             price: 49.97,
             user_id: 1,
@@ -24,8 +26,8 @@ exports.up = knex => knex.schema.createTable('dishes', table => {
         },
         {
             name: 'Spaguetti Gambe',
-            category: 'refeições',
-            description: 'Massa fresca com camarões e pesto.',
+            category: 'meals',
+            description: 'Massa fresca com camarões, pesto e molho de tomate.',
             price: 79.97,
             user_id: 1,
             image_url: 'spaguetti-gambe.png',
@@ -34,7 +36,7 @@ exports.up = knex => knex.schema.createTable('dishes', table => {
         },
         {
             name: 'Torradas de Parma',
-            category: 'refeições',
+            category: 'meals',
             description: 'Presunto de parma e rúcula em um pão com fermentação natural.',
             price: 25.97,
             user_id: 1,
@@ -43,8 +45,8 @@ exports.up = knex => knex.schema.createTable('dishes', table => {
             updated_at: knex.fn.now()
         },
         {
-            name: 'Salada da Casa Branca',
-            category: 'refeições',
+            name: 'Salada Caprese',
+            category: 'meals',
             description: 'Folhas verdes de alface e manjericão, tomate, cebola roxa e pepino.',
             price: 29.97,
             user_id: 1,
@@ -54,7 +56,7 @@ exports.up = knex => knex.schema.createTable('dishes', table => {
         },
         {
             name: 'Prugna Pie',
-            category: 'sobremesas',
+            category: 'desserts',
             description: 'Torta de ameixa com massa amanteigada, polvilho em açucar',
             price: 79.97,
             user_id: 1,
@@ -64,7 +66,7 @@ exports.up = knex => knex.schema.createTable('dishes', table => {
         },
         {
             name: 'Peachy pastrie',
-            category: 'sobremesas',
+            category: 'desserts',
             description: 'Delicioso folheado de pêssego com folhas de hortelã.',
             price: 59.97,
             user_id: 1,
@@ -74,7 +76,7 @@ exports.up = knex => knex.schema.createTable('dishes', table => {
         },
         {
             name: 'Macarons',
-            category: 'sobremesas',
+            category: 'desserts',
             description: 'Farinha de amêndoas, manteiga, claras e açúcar.',
             price: 39.97,
             user_id: 1,
@@ -84,8 +86,8 @@ exports.up = knex => knex.schema.createTable('dishes', table => {
         },
         {
             name: 'Bolo de ameixa',
-            category: 'sobremesas',
-            description: 'Bolo de ameixa fresca cortada em fatias com creme de leite e gostas de baunilha.',
+            category: 'desserts',
+            description: 'Bolo de ameixa fresca fatiada com creme de leite e gostas de baunilha.',
             price: 49.97,
             user_id: 1,
             image_url: 'bolo-de-ameixa.png',
@@ -94,16 +96,17 @@ exports.up = knex => knex.schema.createTable('dishes', table => {
         },
         {
             name: 'Espresso',
-            category: 'bebidas',
+            category: 'drinks',
             description: 'Café cremoso feito na temperatura e pressões perfeitas.',
             price: 15.97,
             user_id: 1,
             image_url: 'espresso.png',
             created_at: knex.fn.now(),
+            updated_at: knex.fn.now()
         },
         {
             name: 'Suco de maracujá',
-            category: 'bebidas',
+            category: 'drinks',
             description: 'Suco de maracujá gelado, cremoso, docinho.',
             price: 13.97,
             user_id: 1,
@@ -113,7 +116,7 @@ exports.up = knex => knex.schema.createTable('dishes', table => {
         },
         {
             name: "Tè d'autonno",
-            category: 'bebidas',
+            category: 'drinks',
             description: 'Chá de anis, canela e limão. Sinta o outono italiano.',
             price: 19.97,
             user_id: 1,
@@ -123,7 +126,7 @@ exports.up = knex => knex.schema.createTable('dishes', table => {
         },
         {
             name: "Vanilla Sky",
-            category: 'bebidas',
+            category: 'drinks',
             description: 'vermute, vodka de baunilha e fatias de maçã.',
             price: 39.97,
             user_id: 1,
