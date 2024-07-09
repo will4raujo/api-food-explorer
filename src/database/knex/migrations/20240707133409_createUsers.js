66e104c5ef5ca25c1f4c15b86c1d8db9f38c5854
willian <willian.araujo1994@gmail.com>
2024-07-09 19:44:36 -0300

    fix: user seed and restarted db

--- src/database/knex/migrations/20240707133409_createUsers.js
+++ src/database/knex/migrations/20240707133409_createUsers.js
@@ -1,27 +1,40 @@
+const { hash } = require("bcryptjs");
 
-exports.up = knex => knex.schema.createTable('users', table => {
-  table.increments('id')
-  table.text('name').notNullable()
-  table.text('email').notNullable()
-  table.text('password').notNullable()
+exports.up = async function (knex) {
+  await knex.schema.createTable('users', table => {
+    table.increments('id');
+    table.text('name').notNullable();
+    table.text('email').notNullable();
+    table.text('password').notNullable();
+    table.enum("role", ["admin", "customer"], { useNative: true, enumName: "roles" })
+      .notNullable().defaultTo("customer");
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+    table.timestamp("updated_at").defaultTo(knex.fn.now());
+  });
 
-  table.enum("role", ["admin", "customer"], {useNative: true, enumName: "roles"})
-  .notNullable().defaultTo("customer")
-  table.timestamp("created_at").default(knex.fn.now())
-  table.timestamp("updated_at").default(knex.fn.now())
-}).then(() => {
-  return knex("users").insert([
+  const hashedPasswordAdmin = await hash("123456", 8);
+  const hashedPasswordCustomer = await hash("123456", 8);
+
+  await knex("users").insert([
     {
-      name: "Semeador",
-      email: "semeador@teste.com",
-      password: "kd65dAnAd8yY4E5a3W",
+      name: "Usuário admin",
+      email: "admin@teste.com",
+      password: hashedPasswordAdmin,
       role: "admin",
       created_at: knex.fn.now(),
       updated_at: knex.fn.now()
     },
-  ])
-})
+    {
+      name: "Usuário Cliente",
+      email: "user@teste.com",
+      password: hashedPasswordCustomer,
+      role: "customer",
+      created_at: knex.fn.now(),
+      updated_at: knex.fn.now()
+    }
+  ]);
+};
 
-
-
-exports.down = knex => knex.schema.dropTable("users")
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
