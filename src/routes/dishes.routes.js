469ef6cb7b445ed7054accd4d4d7963a455f66b2
willian <willian.araujo1994@gmail.com>
2024-07-09 11:48:00 -0300

    fix: adjust in controller and middleware

--- src/routes/dishes.routes.js
+++ src/routes/dishes.routes.js
@@ -15,11 +15,11 @@ const upload = multer(uploadConfig.MULTER);
 dishesRoutes.use(ensureAuthenticated);
 
 dishesRoutes.get('/', verifyUserAuthorization(['admin', 'customer']), dishesController.index);
-dishesRoutes.post('/', verifyUserAuthorization(['admin']), dishesController.create);
+dishesRoutes.post('/', verifyUserAuthorization(['admin']), upload.single('image'), dishesController.create);
 dishesRoutes.get('/search', verifyUserAuthorization(['admin', 'customer']), dishesController.getByTitle);
 dishesRoutes.get('/search/ingredients', verifyUserAuthorization(['admin', 'customer']), dishesController.getByIngredients);
 dishesRoutes.get('/:id', verifyUserAuthorization(['admin', 'customer']), dishesController.getById);
-dishesRoutes.put('/:id', verifyUserAuthorization(['admin']), dishesController.update);
+dishesRoutes.put('/:id', verifyUserAuthorization(['admin']), upload.single('image'), dishesController.update);
 dishesRoutes.delete('/:id', verifyUserAuthorization(['admin']), dishesController.delete);
 dishesRoutes.patch('/image/:id', verifyUserAuthorization(['admin']), upload.single('image'), dishesImagesController.upload);
 
