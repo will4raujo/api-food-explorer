d137fff5b890df5bd4242834cc41268e6af67c5f
willian <willian.araujo1994@gmail.com>
2024-07-19 17:35:51 -0300

    fix: adding render domain in cors

--- src/server.js
+++ src/server.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 const app = express();
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "https://127.0.0.1:5173/", "http://localhost:5174", "https://127.0.0.1:5174/"],
+  origin: ["http://localhost:5173", "https://127.0.0.1:5173/", "http://localhost:5174", "https://127.0.0.1:5174/", "https://api-food-explorer-6zjq.onrender.com"],
   credentials: true
 }));
 app.use(express.json());
